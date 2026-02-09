5e209f71ad7ab9d2cd6622a324093012b54a3162
Nishchay Kumar Bhardwaj <168272378+nkbweb@users.noreply.github.com>
2026-02-10 01:57:41 +0530

    Add files via upload

--- app/schemas/auth.ts
+++ app/schemas/auth.ts
@@ -1,1 +1,12 @@
-abbs
+import z from "zod";
+
+export const signUpSchema=z.object({
+    name:z.string().min(3).max(30),
+    email:z.string().email(),
+    password:z.string().min(8).max(30),
+});
+
+export const loginSchema=z.object({
+    email:z.string().email(),
+    password:z.string().min(1),
+});
